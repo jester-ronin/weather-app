ad77ffffd946c00df2f1950a0dd58a65a2ab7a64
jester-ronin <82144563+jester-ronin@users.noreply.github.com>
2024-05-27 13:05:50 +0300

    Adapted styles for different screens

--- src/components/WeatherCarousel/WeatherCarousel.js
+++ src/components/WeatherCarousel/WeatherCarousel.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import  './weatherCarousel.css';
+
 
 const WeatherCarousel = ({ cities, selectionPictures, temperatureConverter, handleRemoveCity }) => {
     return (
         <Carousel>
             {cities && cities.map(city => (
                 <Carousel.Item key={city.id}>
-                    <img
-                        className="d-block w-100"
-                        src={selectionPictures(city)}
-                        alt={`Slide for ${city.data ? city.data.name : "City Name"}`}
-                    />
-                    <Carousel.Caption>
-                        <h3>{city.data ? city.data.name : "City Name"}</h3>
-                        <p>{city.data ? temperatureConverter(city) + "° " : "Temperature"}</p>
-                        <p>{city.data ? city.data.weather[0].main : "Weather"}</p>
-                        <p>{city.data ? "Wind " + city.data.wind.speed + ' m/s' : "Wind"}</p>
-                    </Carousel.Caption>
-                    <Button variant="danger" onClick={() => handleRemoveCity(city.id)}>Delete</Button>{' '}
+                    <div className='carousel-div'>
+                        <img
+                            className="d-block w-100"
+                            src={selectionPictures(city)}
+                            alt={`Slide for ${city.data ? city.data.name : "City Name"}`}
+                        />
+                        <Card.Text>
+                            <h3>{city.data ? city.data.name : "City Name"}</h3>
+                            <p>{city.data ? temperatureConverter(city) + "° " : "Temperature"}</p>
+                            <p>{city.data ? city.data.weather[0].main : "Weather"}</p>
+                            <p>{city.data ? "Wind " + city.data.wind.speed + ' m/s' : "Wind"}</p>
+                            <Button variant="danger" id='delete-button' onClick={() => handleRemoveCity(city.id)}>Delete</Button>{' '}
+                        </Card.Text>
+
+                    </div>
                 </Carousel.Item>
+                
             ))}
         </Carousel>
     );
